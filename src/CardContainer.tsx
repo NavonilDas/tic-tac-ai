d398650d27598a7e849bb99d576dd97736e1e2b0
Navonil Das <navneeladas@gmail.com>
2020-12-20 20:50:03 +0530

    Card Container

---
+++ src/CardContainer.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Card from './Card';
+
+interface Props {
+    onPlayerChange?: (player: boolean) => void;
+};
+
+interface State {
+    board: ('X' | 'O' | '')[][];
+    turn: boolean; // True for X   
+};
+
+class CardContainer extends React.Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            board: [['', '', ''], ['', '', ''], ['', '', '']],
+            turn: true
+        };
+    }
+
+    render() {
+        return (
+            <div className="card-container">
+                {this.state.board.map((row, i) =>
+                    row.map((ele, j) =>
+                        <Card key={3 * i + j} value={ele} onClick={() => {
+                            if (this.state.board[i][j] !== '') return;
+                            const board = this.state.board;
+                            board[i][j] = this.state.turn ? 'X' : 'O';
+                            this.setState({ board, turn: !this.state.turn });
+                        }} />
+                    )
+                )}
+            </div>
+        );
+    }
+}
+
+
+export default CardContainer;
